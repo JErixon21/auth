fd3c7c2c95ab7582979e308fd8eaec28c5854243
Jake Erickson <jake212e@gmail.com>
2025-02-27 02:27:34 +0000

    Staging, but running into error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,26 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["email"]})
     # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+    if @user != nil
+      # 3. if they know their password -> login is successful
+      # if @user["password"] == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+
+        cookies["name"] = "Cookie Monster"
+
+        flash["notice"] = "Welcome."
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+      # 4. if the user doesn't exist or they don't know their password -> login fails
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
@@ -19,3 +34,5 @@ class SessionsController < ApplicationController
     redirect_to "/login"
   end
 end
+
+
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,12 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+password = "dogs"
+encrypted_password = BCrypt::Password.create("dogs")
+puts encrypted_password
+# $2a$12$fXA/cqPQvBMsJP5RFQrKWujEORrHDU.B6pJXWWlKhtyC5PPHOeC3e
+
 # 2. prepare encrypted string for testing
+puts BCrypt::Password.new(encrypted_password) == "dogs"
+
 # 3. test secret against prepared encrypted string
